4f3395c2bd1ef73f461331cb8e49fb9fef4d46dc
trex81 <trex81@yndex.ru>
2021-11-07 19:57:40 +0300

    12

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace lesson
+   // new_repo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("");
+        }
+    }
+}
